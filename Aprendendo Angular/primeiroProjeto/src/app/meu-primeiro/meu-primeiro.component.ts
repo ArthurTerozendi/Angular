797d9cd46b26edba489547a637a3400e09d8f149
Arthur Terozendi <arthurterozendi@gmail.com>
2020-10-03 14:45:38 -0300

    primeiro projeto com angular 2

---
+++ Aprendendo Angular/primeiroProjeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector : 'meu-primeiro-component',
+    template : ` <h1> Teste </h1>
+        <p> Meu primeiro component com Angular 2! </p>
+    `
+})
+export class MeuPrimeiroComponent {}
