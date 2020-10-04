24af4f8853c60f726dd6f39e5173fb869602aa01
Arthur Terozendi <arthurterozendi@gmail.com>
2020-10-04 12:14:02 -0300

    finalizada aula de modulos

--- Aprendendo Angular/primeiroProjeto/src/app/app.module.ts
+++ Aprendendo Angular/primeiroProjeto/src/app/app.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
