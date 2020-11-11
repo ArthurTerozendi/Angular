23ff6e5f1bf4c9b6cfe16fe9ff9b69e692679eee
Arthur Terozendi <arthurterozendi@gmail.com>
2020-11-11 08:42:32 -0300

    data binding v2.1

--- treinamento-Concert/aula-data-binding/src/app/app.module.ts
+++ treinamento-Concert/aula-data-binding/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,14 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    MatButtonModule,
+    MatBottomSheetModule
+  ]
 })
 export class AppModule { }
