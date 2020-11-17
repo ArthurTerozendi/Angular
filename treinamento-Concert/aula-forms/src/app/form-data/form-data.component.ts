a0fc989f514bdd8ad369ff69e3c230f3e89961cb
Arthur Terozendi <arthurterozendi@gmail.com>
2020-11-17 13:42:36 -0300

    form v3.0

--- treinamento-Concert/aula-forms/src/app/form-data/form-data.component.ts
+++ treinamento-Concert/aula-forms/src/app/form-data/form-data.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-data',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormDataComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        rua: [null, Validators.required],
+        num: [null, Validators.required],
+        complemento: [null],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required],
+        pais: [null, Validators.required]
+      }),
+    })
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.valid);
+      this.httpClient.post('http://httpbin.org/post', JSON.stringify(this.form.value)).subscribe(
+        dados => {
+          console.log(dados)
+          //this.resetarForm();
+        },
+        (error: any) => alert('erro')
+      );
+    } else {
+      this.form.markAllAsTouched();
+    }
+
+  }
+
+  resetarForm() {
+    console.log('resetou')
+    this.form.reset();
+  }
+
+  aplicarCssError(campo){
+    return{
+      'has-error': this.verificarValid(campo)
+    }
+  }
+
+  verificarValid(campo) {
+    return !this.form.get(campo).valid && this.form.get(campo).touched
   }
 
 }
--- treinamento-Concert/aula-forms/src/app/form-data/form-data.module.ts
+++ treinamento-Concert/aula-forms/src/app/form-data/form-data.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { FormDataRoutingModule } from './form-data-routing.module';
 import { FormDataComponent } from './form-data.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [FormDataComponent],
   imports: [
     CommonModule,
-    FormDataRoutingModule
+    FormDataRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class FormDataModule { }
