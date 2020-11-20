79220256674ef020828e60eac84b6efd5515ca37
Arthur Terozendi <arthurterozendi@gmail.com>
2020-11-20 12:28:36 -0300

    http v2.0

--- treinamento-Concert/aula-http/src/app/app-routing.module.ts
+++ treinamento-Concert/aula-http/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'cursos', loadChildren: () => import('src/app/cursos/cursos.module').then(m => m.CursosModule)}
+  {path: 'cursos', loadChildren: () => import('src/app/cursos/cursos.module').then(m => m.CursosModule)},
+  {path: 'rxjs', loadChildren: () => import('src/app/rxjs-poc/rxjs-poc.module').then(m => m.RxjsPocModule)}
 ];
 
 @NgModule({
