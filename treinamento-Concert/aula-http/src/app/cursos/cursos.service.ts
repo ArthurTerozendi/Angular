413b92f1fad8511fe4e164d6d595e08b7b00512c
Arthur Terozendi <arthurterozendi@gmail.com>
2020-11-22 11:21:35 -0300

    http v3.1

--- treinamento-Concert/aula-http/src/app/cursos/cursos.service.ts
+++ treinamento-Concert/aula-http/src/app/cursos/cursos.service.ts
@@ -3,53 +3,16 @@ import { Injectable } from '@angular/core';
 import { Curso } from './models/curso';
 import { delay, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { CrudService } from '../shared/crudService';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CursosService {
-
-  private readonly API = `${ environment.API }cursos`  
+export class CursosService extends CrudService<Curso>{
 
   constructor(
-    private httpClient : HttpClient
-  ) { }
-
-  list() {
-    return this.httpClient.get<Curso[]>(this.API).pipe(
-      delay(2000),
-      //tap(console.log)
-    );
-  }
-
-  private post(curso) {
-    return this.httpClient.post(this.API, curso).pipe(
-      take(1)
-    );
-  }
-
-  private update(curso) {
-    return this.httpClient.put(`${this.API}/${curso.id}`, curso).pipe(
-      take(1)
-    )
-  }
-
-  loadByID(id) {
-    return this.httpClient.get<Curso>(`${this.API}/${id}`).pipe(
-      take(1)
-    )
-  }
-
-  save(curso) {
-    if (!curso.id) {
-      return this.post(curso);
-    }
-    return this.update(curso);
-  }
-
-  remove(cursoId) {
-    return this.httpClient.delete(`${this.API}/${cursoId}`).pipe(
-      take(1)
-    )
-  }
+    protected httpClient : HttpClient
+  ) {
+    super(httpClient, `${ environment.API }cursos`)
+   }
 }
